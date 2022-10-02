b94561a694a57064897594c19975bccabf347106
Robert Topolowski <robert.topolowski@kcl.ac.uk>
2022-10-02 21:25:32 +0100

    Implement prime calculation functionality

--- primes.py
+++ primes.py
@@ -3,4 +3,15 @@
 
 def primes(number_of_primes):
     list = []
+
+    current_number = 2
+    while len(list) < number_of_primes:
+        prime_flag = True
+        for i in range(2, int(current_number / 2) + 1):
+            if current_number % i == 0:
+                prime_flag = False
+        if prime_flag:
+            list.append(current_number)
+        current_number = current_number + 1
+
     return list
